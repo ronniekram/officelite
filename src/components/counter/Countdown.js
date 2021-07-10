d92479b86be7c80f6ff806c59f37a8858a1842d3
Arynn Boniface <arynn.boniface@gmail.com>
2021-07-10 15:01:51 -0400

    counter fully functional, need to keep it from going past zero

--- src/components/counter/Countdown.js
+++ src/components/counter/Countdown.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const Countdown = () => {
   // date 30 days from now
-  const future = new Date(Date.now() + 2592000000).getTime();
+  const future = new Date(Date.now() + 2592000000);
   // const [future, setFuture] = useState(new Date(Date.now() + 2592000000).getTime());
   const [date, setDate] = useState({
     days: 0,
@@ -18,7 +18,7 @@ const Countdown = () => {
   const setTime = () => {
     if (future) {
       const current = new Date().getTime();
-      const waitTime = future - current;
+      const waitTime = future.getTime() - current;
 
       let days = Math.floor(waitTime / (1000 * 60 * 60 * 24));
 
@@ -30,7 +30,7 @@ const Countdown = () => {
 
       const addZeroes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-      // days = `${days}`;
+      days = `${days}`;
       if (addZeroes.includes(hours)) {
         hours = `0${hours}`;
       } else if (addZeroes.includes(minutes)) {
@@ -40,19 +40,17 @@ const Countdown = () => {
       }
 
       setDate({ 
-        days: days, 
-        hours: hours, 
-        minutes: minutes, 
-        seconds: seconds 
+        days: days.toString(), 
+        hours: hours.toString(), 
+        minutes: minutes.toString(), 
+        seconds: seconds.toString() 
       });
     }
   };
 
-    console.log(date)
-
   return (
     <article>
-
+      <h1>Countdown to {future.toDateString()}</h1>
     </article>
   );
 };
